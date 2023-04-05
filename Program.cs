d283bc07f143fa84e12abfa18c29d38dce93a611
BhagyashriSabale <balisabale16@gmail.com>
2023-04-05 13:01:43 +0530

    [Bhagyashri] Adding Welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace WelcomeHome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Home");
+        }
+    }
+}
